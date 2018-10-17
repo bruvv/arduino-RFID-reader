304be3b765bf92bf6130864db836692acbc9b95a
d1slact0r <3063928+d1slact0r@users.noreply.github.com>
2018-10-17 11:36:37 +0200

    Update FixBrickedUID.ino

--- examples/FixBrickedUID/FixBrickedUID.ino
+++ examples/FixBrickedUID/FixBrickedUID.ino
@@ -25,15 +25,15 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN   9     // Configurable, see typical pin layout above
-#define SS_PIN    10    // Configurable, see typical pin layout above
+#define RST_PIN         D1          // Configurable, see typical pin layout above
+#define SS_PIN          D0          // Configurable, see typical pin layout above
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 
 MFRC522::MIFARE_Key key;
 
 void setup() {
-  Serial.begin(9600);  // Initialize serial communications with the PC
+  Serial.begin(115200);  // Initialize serial communications with the PC
   while (!Serial);     // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
   SPI.begin();         // Init SPI bus
   mfrc522.PCD_Init();  // Init MFRC522 card
@@ -46,7 +46,7 @@ void setup() {
 }
 
 void loop() {
-  if ( mfrc522.MIFARE_UnbrickUidSector(false) ) {
+  if ( mfrc522.MIFARE_UnbrickUidSector(true) ) {
     Serial.println(F("Cleared sector 0, set UID to 1234. Card should be responsive again now."));
   }
   delay(1000);
